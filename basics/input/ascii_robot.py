e12f4463f04292debdce25b689ed3491ea7fdfe4
Felix Tudorascu <82870321+TudorascuFelixC@users.noreply.github.com>
2021-05-18 11:56:05 +0100

    Add files via upload

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,11 @@
+# Ask user for eye character 
+
+def ascii_robot():
+  print("Please enter eye character")
+  eye = input() 
+
+  # Display an ascii art robot
+  print("##########")
+  print(f"#  {eye}  {eye}   #")
+  print("#  ----  #")
+  print("##########")
---
+++ basics/output/escape_characters.py
@@ -0,0 +1,10 @@
+#Displaying escape characters
+
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a backslash")
+print("\" Displays double quote")
+print("\' Displays single quote")
+
+# Display a message
+print(" \n\t\t\"I am Beep!\" ")
